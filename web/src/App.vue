15c6cd56bf42b12c539004b3bb2ec62c1c5d7abb
Alan Chen <1535823463@qq.com>
2023-08-11 00:13:41 +0800

    add jwt config and finish account backend

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,10 +7,57 @@
   import NavBar from './components/NavBar.vue'
   import "bootstrap/dist/css/bootstrap.min.css"
   import "bootstrap/dist/js/bootstrap"
+  import $ from "jquery"
 
   export default {
     components: {
       NavBar
+    },
+    setup() {
+      $.ajax({
+        url: "http://127.0.0.1:3000/user/account/token/",
+        type: "post",
+        data: {
+          username: 'alan',
+          password: 'palan',
+        },
+        success(resp) {
+          console.log(resp);
+        },
+        error(resp) {
+          console.log(resp);
+        }
+      })
+
+      $.ajax({
+        url: "http://127.0.0.1:3000/user/account/info/",
+        type: "get",
+        headers: {
+          Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJmMTJhMTk1OGIzNWQ0ODc1YjA1MzFiYTQ3ZjViNDU3YSIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY5MTY3MTY5NiwiZXhwIjoxNjkyODgxMjk2fQ.wovBjEQiHIe-pVsJ8xCUkKbe4I1cLlCEk8EvT8YLq6M"
+        },
+        success(resp) {
+          console.log(resp);
+        },
+        error(resp) {
+          console.log(resp);
+        }
+      })
+
+      $.ajax({
+        url: "http://127.0.0.1:3000/user/account/register/",
+        type: "post",
+        data: {
+          username: 'alan23',
+          password: 'palan23',
+          confirmedPassword: "palan23",
+        },
+        success(resp) {
+          console.log(resp);
+        },
+        error(resp) {
+          console.log(resp);
+        }
+      })
     }
   }
 </script>
