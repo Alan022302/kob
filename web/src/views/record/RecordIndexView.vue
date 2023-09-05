be937d8e2eb2da1887fb3997a8f4b7b5de082ec8
Alan Chen <1535823463@qq.com>
2023-09-05 17:12:13 +0800

    deployed the project online

--- web/src/views/record/RecordIndexView.vue
+++ web/src/views/record/RecordIndexView.vue
@@ -95,7 +95,7 @@
             const pull_page = page => {
                 current_page = page;
                 $.ajax({
-                    url: "http://127.0.0.1:3000/record/getlist/",
+                    url: "https://app5878.acapp.acwing.com.cn/api/record/getlist/",
                     data: {
                         page,
                     },
